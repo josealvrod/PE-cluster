996caaece8e82474c27a89a98ca0299bf551013e
Juanjo Presa <juanjop@gmail.com>
2016-10-06 21:55:09 +0200

    Add install-certs.sh to install script

--- scripts/install-m1.sh
+++ scripts/install-m1.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 /tmp/puppet-enterprise-2016.2.1-el-7-x86_64/puppet-enterprise-installer -c /vagrant/files/master/pe1.conf
+cd /vagrant/certs; ./install-certs.sh
 /opt/puppetlabs/puppet/bin/puppet agent -t
 #sudo /vagrant/files/saved_certs.sh
 #sudo /opt/puppetlabs/puppet/bin/puppet agent -t
